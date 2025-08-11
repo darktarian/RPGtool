642068554346a4acc6d0dde252903a7b9be60bf6
frederic <lepicardfrederic@protonmail.com>
2025-08-11 11:52:58 +0200

    bon j'ai fait ma propore classe de bouton pour les dés

--- src/diceboard.rs
+++ src/diceboard.rs
@@ -12,73 +12,69 @@ pub(crate) fn DiceBoard() -> Element {
     rsx! {
 
         div {  class:"row pb-1 border-bottom border-warning-subtle", div { class:"col", div {class:"text-warning",  "Set classique " } }}
-        div{ class: "row mt-4 mb-4",
-            div {  class:"col",
-                div { class:"btn btn-warning w-75", id:"btn4", onclick: move |_event| {
+        div{ class: "container mt-4 mb-1",
+                div { class:"btn-dice", id:"btn3", onclick: move |_event| {
+                        let d3 = "1d3".parse::<Dice>().unwrap();
+                        sig_result.set(d3.total().to_string());
+                }, "1D3" }
+                div { class:"btn-dice", id:"btn4", onclick: move |_event| {
                         let d4 = "1d4".parse::<Dice>().unwrap();
                         sig_result.set(d4.total().to_string());
                 }, "1D4" }
-            }
-            div { class:"col",
-                div { class:"btn btn-warning w-75", id:"btn6", onclick: move |_event| {
-                        let d6 = "1d6".parse::<Dice>().unwrap(); //Equation::new("1d6").unwrap();
+
+                div { class:"btn-dice", id:"btn4", onclick: move |_event| {
+                        let d5 = "1d5".parse::<Dice>().unwrap();
+                        sig_result.set(d5.total().to_string());
+                }, "1D5" }
+                div { class:"btn-dice", id:"btn6", onclick: move |_event| {
+                        let d6 = "1d6".parse::<Dice>().unwrap();
                         sig_result.set(d6.total().to_string());
                 }, "1D6" }
-            }
-            div {  class:"col",
-                div { class:"btn btn-warning w-75", id:"btn8", onclick: move |_event| {
+                div { class:"btn-dice", id:"btn7", onclick: move |_event| {
+                        let d7 = "1d7".parse::<Dice>().unwrap();
+                        sig_result.set(d7.total().to_string());
+                }, "1D6" }
+                div { class:"btn-dice", id:"btn8", onclick: move |_event| {
                         let d8 = "1d8".parse::<Dice>().unwrap();
                         sig_result.set(d8.total().to_string());
                 }, "1D8" }
-            }
-            div {  class:"col",
-                div { class:"btn btn-warning w-75", id:"btn10", onclick: move |_event| {
+                div { class:"btn-dice", id:"btn10", onclick: move |_event| {
                         let d10 = "1d10".parse::<Dice>().unwrap();
                         sig_result.set(d10.total().to_string());
                 }, "1D10" }
-            }
-            div {  class:"col",
-                div { class:"btn btn-warning w-75", id:"btn12", onclick: move |_event| {
+        }
+        div{ class:"container mt-1 mb-1",
+                div { class:"btn-dice", id:"btn12", onclick: move |_event| {
                         let d12 = "1d12".parse::<Dice>().unwrap();
                         sig_result.set(d12.total().to_string());
                 }, "1D12" }
-            }
-            div {  class:"col",
-                div { class:"btn btn-warning w-75", id:"btn14", onclick: move |_event| {
+                div { class:"btn-dice", id:"btn14", onclick: move |_event| {
                         let d14 = "1d14".parse::<Dice>().unwrap();
                         sig_result.set(d14.total().to_string());
                 }, "1D14" }
-            }
-
-        }
-        div{ class:"row mt-2 mb-2",
-            div {  class:"col-2",
-                div { class:"btn btn-warning w-75", id:"btn16", onclick: move |_event| {
+                div { class:"btn-dice", id:"btn16", onclick: move |_event| {
                         let d12 = "1d16".parse::<Dice>().unwrap();
                         sig_result.set(d12.total().to_string());
                 }, "1D16" }
-            }
-            div {  class:"col-2",
-                div { class:"btn btn-warning w-75", id:"btn20", onclick: move |_event| {
+                div { class:"btn-dice", id:"btn20", onclick: move |_event| {
                         let d20 = "1d20".parse::<Dice>().unwrap();
                         sig_result.set(d20.total().to_string());
                 }, "1D20" }
-            }
-            div {  class:"col-2",
-                div { class:"btn btn-warning w-75", id:"btn24", onclick: move |_event| {
+                div { class:"btn-dice", id:"btn24", onclick: move |_event| {
                         let d24 = "1d24".parse::<Dice>().unwrap();
                         sig_result.set(d24.total().to_string());
                 }, "1D24" }
-            }
-             div {  class:"col-2",
-                div { class:"btn btn-warning w-75", id:"btn30", onclick: move |_event| {
+                div { class:"btn-dice", id:"btn30", onclick: move |_event| {
                         let d30 = "1d30".parse::<Dice>().unwrap();
                         sig_result.set(d30.total().to_string());
                 }, "1D30" }
-            }
+                div { class:"btn-dice", id:"btn100", onclick: move |_event| {
+                        let d100 = "1d100".parse::<Dice>().unwrap();
+                        sig_result.set(d100.total().to_string());
+                }, "1D100" }
         }
         div {  class:"row mb-4 pb-1 border-bottom border-warning-subtle", div { class:"col", div {class:"text-warning",  "Dés custom " } }}
-        div { class:"row mb-2 mt-2",
+        div { class:"row mb-2 mt-4",
             div { class:"col-5",
                 input { class:"mr-2 form-control form-control-sm",
                     type:"text",
